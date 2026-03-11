3bb209b8e32c1725fe7f2b15031d3b6c8f120b38
Michu <mikolorzech@gmail.com>
2026-03-11 14:10:14 +0100

    Ulepszone taski

---
+++ lib/shared/modules/task.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class Task {
+  final int id;
+  final String title;
+  final String category;
+  final String group;
+  final String description;
+  final int status;
+  final DateTime date;
+  final TimeOfDay timeStart;
+  final TimeOfDay timeEnd;
+
+  Task({
+    required this.id,
+    required this.title,
+    required this.status,
+    required this.category,
+    required this.group,
+    required this.description,
+    required this.date,
+    this.timeStart = const TimeOfDay(hour: 0, minute: 0),
+    this.timeEnd = const TimeOfDay(hour: 0, minute: 0),
+  });
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? "Null",
+      status: json['status'] ?? 0,
+      date: DateTime.parse(json['date']??DateTime.now().toString()),
+      category: json['category']??"Null",
+      group: json['group'] ?? "Null",
+      description: json['description']??"Null",
+      timeStart: TimeOfDay(
+        hour: json['timeStart']['hour'],
+        minute: json['timeStart']['minute'],
+      ),
+      timeEnd: TimeOfDay(
+        hour: json['timeEnd']['hour'],
+        minute: json['timeEnd']['minute'],
+      ),
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "title": title,
+      "status": status,
+      "date": formatDate(date),
+      "category": category,
+      "group": group,
+      "description": description,
+      "timeStart": {"hour": timeStart.hour, "minute": timeStart.minute},
+      "timeEnd": {"hour": timeEnd.hour, "minute": timeEnd.minute},
+    };
+  }
+
+  Task copyWith({
+    int? id,
+    String? title,
+    int? status,
+    DateTime? date,
+    String? category,
+    String? group,
+    String? description,
+    TimeOfDay? timeStart,
+    TimeOfDay? timeEnd,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      status: status ?? this.status,
+      date: _validateDate(date ?? this.date),
+      category: category ?? this.category,
+      group: group ?? this.group,
+      description: description ?? this.description,
+      timeStart: timeStart ?? this.timeStart,
+      timeEnd: timeEnd ?? this.timeEnd,
+    );
+  }
+
+  DateTime _validateDate(DateTime date) {
+    if (date.isBefore(
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day),
+    )) {
+      return DateTime(
+        DateTime.now().year,
+        DateTime.now().month,
+        DateTime.now().day,
+      );
+    } else {
+      return DateTime(date.year, date.month, date.day);
+    }
+  }
+
+  String formatDate(DateTime date) {
+    String m = date.month.toString().padLeft(2, '0');
+    String d = date.day.toString().padLeft(2, '0');
+    return "${date.year}-$m-$d";
+  }
+}
